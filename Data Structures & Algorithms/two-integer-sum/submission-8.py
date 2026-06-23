ae895ccee8dbc46d038a0d29953d11ed79cc851a
Francisco Salazar <55640491+Lumexio@users.noreply.github.com>
2026-06-23 14:44:57 -0600

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashdata=defaultdict(object)
+
+        for i,n in enumerate(nums):
+            comp=target-n
+            if comp in hashdata:
+                return [hashdata[comp],i]
+            else:
+                hashdata[n]=i
+        return [0,0]
